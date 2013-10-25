be2bf39ccd7329036a9efb417100373257182788
Andrew Hill <andrewhill157@gmail.com>
2013-10-25 12:56:45 -0400

    Added smooth scrolling navigation to menu buttons.

--- js/menuButtonCallbacks.js
+++ js/menuButtonCallbacks.js
@@ -1,17 +1,24 @@
-$(document).ready(function() {
+$(document).ready(function() { 
+    // Smooth scrolling functionality for each navigation button
+    var scrolltime = 250; // time spent scrolling to id
     $("#BioButton").on('click', function() {
-        document.getElementById("Bio").scrollIntoView();
+        $('html, body').animate({
+        scrollTop: $("#Bio").offset().top}, scrolltime);
     });
     
+    
     $("#ResearchButton").on('click', function() {
-        document.getElementById("Research").scrollIntoView();
+        $('html, body').animate({
+        scrollTop: $("#Research").offset().top}, scrolltime);
     });
     
     $("#ProjectsButton").on('click', function() {
-        document.getElementById("Projects").scrollIntoView();
+        $('html, body').animate({
+        scrollTop: $("#Projects").offset().top}, scrolltime);
     });
     
     $("#CVButton").on('click', function() {
-        document.getElementById("CV").scrollIntoView();
+        $('html, body').animate({
+        scrollTop: $("#CV").offset().top}, scrolltime);
     });
 });
