f17637ecd28c9efbf4c211ac98c1483726233d89
Andrew Hill <andrewhill157@gmail.com>
2014-08-27 20:06:38 -0400

    New blog post on SSH config. Revised code formatting.

--- new_post.py
+++ new_post.py
@@ -7,31 +7,27 @@ import sys
 
 parser = argparse.ArgumentParser('Generate a markdown file for new blog post.')
 parser.add_argument('title', help='Title of post.') 
-parser.add_argument('tag', help='Tag to use for post category.')
+parser.add_argument('tags', help='Tags to use for post.')
 args = parser.parse_args()
 
 header = \
 """---
 title: %s
 layout: post
-thumbnail:
 tags: %s
 ---
-""" % (args.title, args.tag)
+""" % (args.title, args.tags)
 
 date = datetime.datetime.now()
 slug = args.title.lower().replace(' ', '-').replace('.', '')
-tag_path = os.path.join('blog/_posts', args.tag.lower().replace(' ', '_'))
-file_name = os.path.join(tag_path, '%s-%s-%s-%s.md' % (date.year, date.month, date.day, slug))
+posts_path = 'blog/_posts'
+file_name = os.path.join(posts_path, '%s-%s-%s-%s.md' % (date.year, date.month, date.day, slug))
 
 if os.path.exists(file_name):
     decision = raw_input('A post with this name exists! Do you want to overwrite? ')
 
     if 'y' not in decision.lower():
         sys.exit(1)
-   
-if not os.path.exists(tag_path):    
-    os.mkdir(tag_path)
 
 with open(file_name, 'w') as f:
     f.write(header)
