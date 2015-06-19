35980f6ab595e8aac0f48d909b5158739ff60ebf
Andrew Hill <andrewhill157@gmail.com>
2015-06-18 23:54:53 -0700

    Minor typo fix.

--- new_post.py
+++ new_post.py
@@ -8,6 +8,7 @@ import sys
 parser = argparse.ArgumentParser('Generate a markdown file for new blog post.')
 parser.add_argument('title', help='Title of post.') 
 parser.add_argument('tags', help='Tags to use for post.')
+parser.add_argument('--images', action='store_true', help='Make a directory for images.')
 args = parser.parse_args()
 
 header = \
@@ -20,8 +21,14 @@ tags: %s
 
 date = datetime.datetime.now()
 slug = args.title.lower().replace(' ', '-').replace('.', '')
-posts_path = 'blog/_posts'
+posts_path = 'blog/_posts/'
+images_path = 'images/posts/'
+
 file_name = os.path.join(posts_path, '%s-%s-%s-%s.md' % (date.year, date.month, date.day, slug))
+images_path = os.path.join(images_path, '%s-%s-%s-%s/' % (date.year, date.month, date.day, slug))
+
+if args.images:
+    os.mkdir(images_path)
 
 if os.path.exists(file_name):
     decision = raw_input('A post with this name exists! Do you want to overwrite? ')
