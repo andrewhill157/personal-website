67be096e07dce73a326c4ed8073400bf2e4b2222
Andrew Hill <andrewhill157@gmail.com>
2018-08-02 12:13:49 -0700

    Updates publications.

--- images/publications_thumbnails/generate_thumbnails.sh
+++ images/publications_thumbnails/generate_thumbnails.sh
@@ -1,3 +1,10 @@
 for file in ../../documents/publications/*.pdf; do
-	convert -density 200 ${file}[0] $(basename ${file%.pdf}.png)
+	output_file=$(basename ${file%.pdf}.png)
+
+	if [ ! -f $output_file ]; then
+		convert -density 200 ${file}[0] $output_file
+	fi
 done
+
+# note that for the mouse atac one I had to compress with this tool https://compresspng.com/
+# not sure why the PNG ends up being so big...
